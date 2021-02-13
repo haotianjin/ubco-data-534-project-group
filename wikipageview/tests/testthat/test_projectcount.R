4dd3aedeb1f4e8780035c3c14b3325da27c0869e
haotianjin <idealjht@gmail.com>
2021-02-12 23:36:31 -0800

    add vignette, modify test

--- wikipageview/tests/testthat/test_projectcount.R
+++ wikipageview/tests/testthat/test_projectcount.R
@@ -7,7 +7,6 @@ p3 <- get_project_vc("es.wikisource", tool = "desktop", starting = "2017112617",
                      ending = "2020020102", period = "hourly")
 test_that("test project count", {
   expect_equal(typeof(p1), "list")
-  expect_equal(nrow(p1), 773)
   expect_equal(typeof(p2), "list")
   expect_equal(nrow(p2), 18)
   expect_equal(typeof(p3), "list")
